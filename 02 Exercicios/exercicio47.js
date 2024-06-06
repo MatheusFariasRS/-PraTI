6e24c1b48c10f46645bcbbb11c0f27b338d75298
Matheus Farias <matheusaf.contato@gmail.com>
2024-06-06 17:21:48 -0300

    inicio exercicio47

--- 02 Exercicios/exercicio47.js
+++ 02 Exercicios/exercicio47.js
@@ -1,23 +1,68 @@
-const filmes = [
-    { titulo: "Tropa de Elite", genero: "Ação" },
-    { titulo: "Capitão Fantástico", genero: "Drama" },
-    { titulo: "O Poderoso Chefão", genero: "Crime" },
-    { titulo: "O Menino do Pijama Listrado", genero: "Drama" },
-    { titulo: "High School Musical", genero: "Musical" },
-    { titulo: "O Senhor dos Anéis: Sociedade do Anel", genero: "Fantasia" },
-    { titulo: "Clube da Luta", genero: "Drama" },
-];
+const marombaStore = {
+    Produto: 'Whey', Preco: 99, Estoque: 1200
+};
+  
+function multiplicacao(m, ...p) {
+    console.log(m)
+    console.log(p)
+    let qtddEsoque = {};
+    let resultado = 0
 
-let contagemGeneros = {};
+    p.forEach(v => resultado += m * v)
 
-filmes.forEach(filme => {
-    if (contagemGeneros[filme.genero]) {
-        contagemGeneros[filme.genero]++;
-    } else {
-        contagemGeneros[filme.genero] = 1;
-    }
-});
-
-for (let genero in contagemGeneros) {
-    console.log(`Existem ${contagemGeneros[genero]} filmes no gênero ${genero}.`);
+    return resultado
 }
+
+console.log(multiplicacao(5, 7, 12, 3, 49))
+
+
+/*
+
+function transformeObj(array) {
+    let newObj = {};
+    array.forEach(property => {
+        if (newObj[property]) {
+            newObj[property] += valor;
+        } else {
+            newObj[property] = valor;
+        }
+
+    });
+
+}
+
+
+const professor = {
+    nome: "Tony Stark",
+    materia: "Matemática",
+    notas: {
+        aluno1: 3.5,
+        aluno2: 4.0,
+        aluno3: 2.8
+    }
+};
+
+let somaNotas = 0;
+let numeroDeAlunos = 0;
+
+for (let aluno in professor.notas) {
+    somaNotas += professor.notas[aluno];
+    numeroDeAlunos++;
+}
+
+let media = somaNotas / numeroDeAlunos;
+
+console.log(`A média das notas é ${media.toFixed(2)}.`);
+console.log(media >= 3.0 ? `${professor.nome} está acima da média de aprovação.` : `${professor.nome} está abaixo da média de aprovação.`);
+
+Crie uma função que transforme um objeto de entrada aplicando uma função
+fornecida a cada uma das propriedades do objeto, retornando um novo objeto com os
+resultados.
+ */
+
+/**
+ função
+ transfomr um objeto
+ aplicar a funçao as propriedades do objeto
+ retornar um novo objeto
+ */
